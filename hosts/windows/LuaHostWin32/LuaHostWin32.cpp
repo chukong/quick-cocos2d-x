dda06aa5816bde6aa5171a171b73f85f0842130c
YuLei Liao <dualface@gmail.com>
2012-09-02 04:40:18 +0800

    update win32 project

--- hosts/windows/LuaHostWin32/LuaHostWin32.cpp
+++ hosts/windows/LuaHostWin32/LuaHostWin32.cpp
@@ -9,6 +9,14 @@
 USING_NS_CC;
 
 const string getCommandLineArg(int index);
+LRESULT WindowProc(UINT message, WPARAM wParam, LPARAM lParam, BOOL* pProcessed);
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+void restart(float width = 0, float height = 0);
+
+HWND s_hWnd = NULL;
+float s_startupWidth = 480;
+float s_startupHeight = 320;
+BOOL s_useConsole = FALSE;
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
@@ -21,12 +29,8 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     // create the application instance
     AppDelegate app;
 
-    float width = 480;
-    float height = 320;
-
     // parse command line arguments
     int index = 1;
-    bool open_console = false;
     while (index < __argc)
     {
         const string arg = getCommandLineArg(index);
@@ -40,15 +44,15 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
                 string s_width, s_height;
                 s_width.assign(size, 0, pos);
                 s_height.assign(size, pos + 1, size.length() - pos);
-                width = (float)atoi(s_width.c_str());
-                height = (float)atoi(s_height.c_str());
-                if (width < 480) width = 480;
-                if (height < 320) height = 320;
+                s_startupWidth = (float)atoi(s_width.c_str());
+                s_startupHeight = (float)atoi(s_height.c_str());
+                if (s_startupWidth < 480) s_startupWidth = 480;
+                if (s_startupHeight < 320) s_startupHeight = 320;
             }
         }
         else if (arg.compare("--console") == 0)
         {
-            open_console = true;
+            s_useConsole = true;
         }
         else
         {
@@ -58,7 +62,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
         index++;
     }
 
-    if (open_console)
+    if (s_useConsole)
     {
         AllocConsole();
         // freopen("CONIN$", "r", stdin);
@@ -75,10 +79,16 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     }
 
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
-    eglView->setFrameSize(width, height);
+    
+    eglView->setMenuResource(MAKEINTRESOURCE(IDC_LUAHOSTWIN32));
+    eglView->setWndProc(WindowProc);
+    eglView->setFrameSize(s_startupWidth, s_startupHeight);
+
+    s_hWnd = eglView->getHWnd();
+
     int ret = CCApplication::sharedApplication()->run();
 
-    if (open_console)
+    if (s_useConsole)
     {
         FreeConsole();
     }
@@ -97,183 +107,142 @@ const string getCommandLineArg(int index)
     return s;
 }
 
-//
-//// 全局变量:
-//TCHAR szWindowClass[MAX_LOADSTRING];			// 主窗口类名
-//
-//// 此代码模块中包含的函数的前向声明:
-//ATOM				MyRegisterClass(HINSTANCE hInstance);
-//BOOL				InitInstance(HINSTANCE, int);
-//LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
-//INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
-//
-//int APIENTRY _tWinMain(HINSTANCE hInstance,
-//                     HINSTANCE hPrevInstance,
-//                     LPTSTR    lpCmdLine,
-//                     int       nCmdShow)
-//{
-//	UNREFERENCED_PARAMETER(hPrevInstance);
-//	UNREFERENCED_PARAMETER(lpCmdLine);
-//
-// 	// TODO: 在此放置代码。
-//	MSG msg;
-//	HACCEL hAccelTable;
-//
-//	// 初始化全局字符串
-//	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-//	LoadString(hInstance, IDC_LUAHOSTWIN32, szWindowClass, MAX_LOADSTRING);
-//	MyRegisterClass(hInstance);
-//
-//	// 执行应用程序初始化:
-//	if (!InitInstance (hInstance, nCmdShow))
-//	{
-//		return FALSE;
-//	}
-//
-//	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LUAHOSTWIN32));
-//
-//	// 主消息循环:
-//	while (GetMessage(&msg, NULL, 0, 0))
-//	{
-//		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-//		{
-//			TranslateMessage(&msg);
-//			DispatchMessage(&msg);
-//		}
-//	}
-//
-//	return (int) msg.wParam;
-//}
-//
-//
-//
-////
-////  函数: MyRegisterClass()
-////
-////  目的: 注册窗口类。
-////
-////  注释:
-////
-////    仅当希望
-////    此代码与添加到 Windows 95 中的“RegisterClassEx”
-////    函数之前的 Win32 系统兼容时，才需要此函数及其用法。调用此函数十分重要，
-////    这样应用程序就可以获得关联的
-////    “格式正确的”小图标。
-////
-//ATOM MyRegisterClass(HINSTANCE hInstance)
-//{
-//	WNDCLASSEX wcex;
-//
-//	wcex.cbSize = sizeof(WNDCLASSEX);
-//
-//	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-//	wcex.lpfnWndProc	= WndProc;
-//	wcex.cbClsExtra		= 0;
-//	wcex.cbWndExtra		= 0;
-//	wcex.hInstance		= hInstance;
-//	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LUAHOSTWIN32));
-//	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-//	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-//	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_LUAHOSTWIN32);
-//	wcex.lpszClassName	= szWindowClass;
-//	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-//
-//	return RegisterClassEx(&wcex);
-//}
-//
-////
-////   函数: InitInstance(HINSTANCE, int)
-////
-////   目的: 保存实例句柄并创建主窗口
-////
-////   注释:
-////
-////        在此函数中，我们在全局变量中保存实例句柄并
-////        创建和显示主程序窗口。
-////
-//BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-//{
-//   HWND hWnd;
-//
-//   hInst = hInstance; // 将实例句柄存储在全局变量中
-//
-//   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-//      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-//
-//   if (!hWnd)
-//   {
-//      return FALSE;
-//   }
-//
-//   ShowWindow(hWnd, nCmdShow);
-//   UpdateWindow(hWnd);
-//
-//   return TRUE;
-//}
-//
-////
-////  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
-////
-////  目的: 处理主窗口的消息。
-////
-////  WM_COMMAND	- 处理应用程序菜单
-////  WM_PAINT	- 绘制主窗口
-////  WM_DESTROY	- 发送退出消息并返回
-////
-////
-//LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-//{
-//	int wmId, wmEvent;
-//	PAINTSTRUCT ps;
-//	HDC hdc;
-//
-//	switch (message)
-//	{
-//	case WM_COMMAND:
-//		wmId    = LOWORD(wParam);
-//		wmEvent = HIWORD(wParam);
-//		// 分析菜单选择:
-//		switch (wmId)
-//		{
-//		case IDM_ABOUT:
-//			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-//			break;
-//		case IDM_EXIT:
-//			DestroyWindow(hWnd);
-//			break;
-//		default:
-//			return DefWindowProc(hWnd, message, wParam, lParam);
-//		}
-//		break;
-//	case WM_PAINT:
-//		hdc = BeginPaint(hWnd, &ps);
-//		// TODO: 在此添加任意绘图代码...
-//		EndPaint(hWnd, &ps);
-//		break;
-//	case WM_DESTROY:
-//		PostQuitMessage(0);
-//		break;
-//	default:
-//		return DefWindowProc(hWnd, message, wParam, lParam);
-//	}
-//	return 0;
-//}
-//
-//INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-//{
-//	UNREFERENCED_PARAMETER(lParam);
-//	switch (message)
-//	{
-//	case WM_INITDIALOG:
-//		return (INT_PTR)TRUE;
-//
-//	case WM_COMMAND:
-//		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-//		{
-//			EndDialog(hDlg, LOWORD(wParam));
-//			return (INT_PTR)TRUE;
-//		}
-//		break;
-//	}
-//	return (INT_PTR)FALSE;
-//}
+void restart(float width, float height)
+{
+    PostMessage(s_hWnd, WM_CLOSE, NULL, NULL);
+
+    STARTUPINFO si;
+    memset(&si, 0, sizeof(si));
+
+    PROCESS_INFORMATION pi;
+    memset(&pi, 0, sizeof(pi));
+
+    char buff[MAX_PATH + 1];
+    memset(buff, 0, sizeof(char) * (MAX_PATH + 1));
+
+    const string path = getCommandLineArg(0);
+
+    if (width > 0 && height > 0)
+    {
+        s_startupWidth = width;
+        s_startupHeight = height;
+    }
+    sprintf_s(buff, MAX_PATH, "\"%s\" --size %dx%d", path.c_str(), (int)s_startupWidth, (int)s_startupHeight);
+
+    if (s_useConsole)
+    {
+        strcat_s(buff, " --console");
+    }
+
+    //CCLOG("RESTART: %s", buff);
+
+    WCHAR cmd[MAX_PATH + 1];
+    memset(cmd, 0, sizeof(WCHAR) * (MAX_PATH + 1));
+    MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, buff, strlen(buff), cmd, MAX_PATH);
+
+    CreateProcess(NULL,
+                  cmd,
+                  NULL,
+                  NULL,
+                  FALSE,
+                  NORMAL_PRIORITY_CLASS,
+                  NULL,
+                  NULL,
+                  &si,
+                  &pi);
+}
+
+LRESULT WindowProc(UINT message, WPARAM wParam, LPARAM lParam, BOOL* pProcessed)
+{
+    int wmId, wmEvent;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 分析菜单选择:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_ABOUTBOX), s_hWnd, About);
+			break;
+
+        case ID_FILE_RESTART:
+            restart();
+            break;
+
+        case ID_VIEW_IPHONE4:
+            restart(960, 640);
+            break;
+
+        case ID_VIEW_IPAD:
+            restart(1024, 768);
+            break;
+
+        case ID_VIEW_NEWIPAD:
+            restart(2048, 1536);
+            break;
+
+        case ID_VIEW_ANDROID1:
+            restart(854, 480);
+            break;
+
+        case ID_VIEW_ANDROID2:
+            restart(800, 480);
+            break;
+
+        case ID_VIEW_ANDROID3:
+            restart(1024, 600);
+            break;
+
+        case ID_VIEW_ANDROID4:
+            restart(1280, 720);
+            break;
+
+        case ID_VIEW_ANDROID5:
+            restart(1280, 800);
+            break;
+
+		case IDM_EXIT:
+			DestroyWindow(s_hWnd);
+			break;
+
+        default:
+            return 0;
+		}
+        break;
+
+    case WM_KEYDOWN:
+        if (wParam == VK_F5)
+        {
+            restart();
+        }
+        break;
+
+    default:
+        return 0;
+	}
+
+    *pProcessed = TRUE;
+    return 0;
+}
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
