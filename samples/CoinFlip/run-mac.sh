523d7bb71fbcf1a6a85ebce691e19e192b8e2c42
dualface <dualface@gmail.com>
2013-02-10 16:08:51 +0800

    change directories struct

--- samples/CoinFlip/run-mac.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-"$DIR"/../../bin/mac/LuaHostMac.app/Contents/MacOS/LuaHostMac -workdir "$DIR" -file scripts/main.lua -size 960x640
-
