74bfc3de4a36d8e8142df81f8c125573a279a6f5
dualface <dualface@gmail.com>
2013-05-09 00:03:21 +0800

    add tools for windows

---
+++ bin/make_framework_package.bat
@@ -0,0 +1,8 @@
+@echo off
+set DIR=%~dp0
+cd "%DIR%\..\"
+php %DIR%\lib\compile_scripts.php -zip -x framework.server -p framework framework lib\framework_precompiled\framework_precompiled
+
+copy lib\framework_precompiled\framework_precompiled.zip sample\CoinFlip\res\
+copy lib\framework_precompiled\framework_precompiled.zip sample\Benchmark\res\
+copy lib\framework_precompiled\framework_precompiled.zip template\PROJECT_TEMPLATE_01\res\
