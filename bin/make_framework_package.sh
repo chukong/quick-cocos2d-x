a7f4d078bdcc2650b6fc97a17e3ce0f36b9ff96f
dualface <dualface@gmail.com>
2013-05-08 10:07:46 +0800

    update tools

--- bin/make_framework_package.sh
+++ bin/make_framework_package.sh
@@ -3,5 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/../"
 "$DIR/compile_scripts.sh" -zip -x framework.server -p framework framework lib/framework_precompiled/framework_precompiled
 
-cp lib/framework_precompiled/framework_precompiled.zip samples/CoinFlip/res/
-cp lib/framework_precompiled/framework_precompiled.zip samples/Benchmark/res/
+cp lib/framework_precompiled/framework_precompiled.zip sample/CoinFlip/res/
+cp lib/framework_precompiled/framework_precompiled.zip sample/Benchmark/res/
