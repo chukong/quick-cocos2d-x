b2e8f32d2c68f4b311229f97be1bc75651c9f3fa
dualface <dualface@gmail.com>
2013-05-30 14:17:15 +0800

    update chipmunk

---
+++ lib/third_party/third_party_luabinding.cpp
@@ -0,0 +1,10 @@
+
+#include "third_party_luabinding.h"
+#include "chipmunk/luabinding/CCPhysicsWorld_luabinding.h"
+
+TOLUA_API int luaopen_third_party_luabinding(lua_State* tolua_S)
+{
+    luaopen_CCPhysicsWorld_luabinding(tolua_S);
+
+    return 1;
+};
