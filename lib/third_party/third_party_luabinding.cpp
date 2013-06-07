164f65ee6ea032472af8dacec796d8476d6f6cba
dualface <dualface@gmail.com>
2013-06-07 11:46:28 +0800

    fix third_party compile errors

--- lib/third_party/third_party_luabinding.cpp
+++ lib/third_party/third_party_luabinding.cpp
@@ -1,12 +1,12 @@
 
 #include "third_party_luabinding.h"
 #include "chipmunk/luabinding/CCPhysicsWorld_luabinding.h"
-// #include "CSArmature/luabinding/CSArmature_luabinding.h"
+#include "CSArmature/luabinding/CSArmature_luabinding.h"
 
 TOLUA_API int luaopen_third_party_luabinding(lua_State* tolua_S)
 {
     luaopen_CCPhysicsWorld_luabinding(tolua_S);
-    // luaopen_CSArmature_luabinding(tolua_S);
+    luaopen_CSArmature_luabinding(tolua_S);
 
     return 1;
 };
