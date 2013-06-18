2bd9d451e98186a9a278d053dc7fedd31422f21e
dualface <dualface@gmail.com>
2013-06-18 16:19:11 +0800

    update proj.win32 for VS2010

--- player/proj.win32/update_vs2010_project.sh
+++ player/proj.win32/update_vs2010_project.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DIR=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
-# cp $DIR/quick-x-player_2012.vcxproj $DIR/quick-x-player_2010.vcxproj
+cp $DIR/quick-x-player_2012.vcxproj $DIR/quick-x-player_2010.vcxproj
 
 cat $DIR/quick-x-player_2012.sln | sed 's/Microsoft Visual Studio Solution File, Format Version 12.00/Microsoft Visual Studio Solution File, Format Version 11.00/' | sed 's/Visual Studio 2012/Visual Studio 2010/' | sed 's/_2012\.vcxproj/_2010\.vcxproj/' > $DIR/quick-x-player_2010.sln
 
