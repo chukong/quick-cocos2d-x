a1d178fb0c55bafae50de5e0f9a0a0e5520555e5
linguofeng <linguofeng@msn.com>
2013-06-19 10:10:03 +0800

    fix android compile errors

---
+++ lib/cocos2d-x/extensions/Android.mk
@@ -0,0 +1,69 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := cocos_extension_static
+
+LOCAL_MODULE_FILENAME := libextension
+
+LOCAL_SRC_FILES := AssetsManager/AssetsManager.cpp \
+CCBReader/CCBFileLoader.cpp \
+CCBReader/CCBReader.cpp \
+CCBReader/CCControlButtonLoader.cpp \
+CCBReader/CCControlLoader.cpp \
+CCBReader/CCLabelBMFontLoader.cpp \
+CCBReader/CCLabelTTFLoader.cpp \
+CCBReader/CCLayerColorLoader.cpp \
+CCBReader/CCLayerGradientLoader.cpp \
+CCBReader/CCLayerLoader.cpp \
+CCBReader/CCMenuItemImageLoader.cpp \
+CCBReader/CCMenuItemLoader.cpp \
+CCBReader/CCNodeLoader.cpp \
+CCBReader/CCNodeLoaderLibrary.cpp \
+CCBReader/CCParticleSystemQuadLoader.cpp \
+CCBReader/CCScale9SpriteLoader.cpp \
+CCBReader/CCScrollViewLoader.cpp \
+CCBReader/CCSpriteLoader.cpp \
+CCBReader/CCBAnimationManager.cpp \
+CCBReader/CCBKeyframe.cpp \
+CCBReader/CCBSequence.cpp \
+CCBReader/CCBSequenceProperty.cpp \
+CCBReader/CCBValue.cpp \
+CCBReader/CCData.cpp \
+CCBReader/CCNode+CCBRelativePositioning.cpp \
+GUI/CCControlExtension/CCControl.cpp \
+GUI/CCControlExtension/CCControlButton.cpp \
+GUI/CCControlExtension/CCControlColourPicker.cpp \
+GUI/CCControlExtension/CCControlHuePicker.cpp \
+GUI/CCControlExtension/CCControlSaturationBrightnessPicker.cpp \
+GUI/CCControlExtension/CCControlSlider.cpp \
+GUI/CCControlExtension/CCControlSwitch.cpp \
+GUI/CCControlExtension/CCControlUtils.cpp \
+GUI/CCControlExtension/CCInvocation.cpp \
+GUI/CCControlExtension/CCScale9Sprite.cpp \
+GUI/CCControlExtension/CCControlPotentiometer.cpp \
+GUI/CCControlExtension/CCControlStepper.cpp \
+GUI/CCScrollView/CCScrollView.cpp \
+GUI/CCScrollView/CCTableView.cpp \
+GUI/CCScrollView/CCTableViewCell.cpp \
+GUI/CCScrollView/CCSorting.cpp \
+GUI/CCEditBox/CCEditBox.cpp \
+GUI/CCEditBox/CCEditBoxImplAndroid.cpp
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_curl_static
+
+LOCAL_CFLAGS += -DCC_ENABLE_CHIPMUNK_INTEGRATION=1
+LOCAL_EXPORT_CFLAGS += -DCC_ENABLE_CHIPMUNK_INTEGRATION=1
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) \
+                           $(LOCAL_PATH)/CCBReader \
+                           $(LOCAL_PATH)/GUI/CCControlExtension \
+                           $(LOCAL_PATH)/GUI/CCScrollView \
+                           $(LOCAL_PATH)/GUI/CCEditBox \
+                    
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,cocos2dx)
+$(call import-module,CocosDenshion/android)
+$(call import-module,cocos2dx/platform/third_party/android/prebuilt/libcurl)
--- lib/proj.android/Android.mk
+++ lib/proj.android/Android.mk
@@ -5,19 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := quickcocos2dx
 LOCAL_MODULE_FILENAME := libquickcocos2dx
 
-LOCAL_SRC_FILES := ../cocos2d-x/extensions/GUI/CCControlExtension/CCScale9Sprite.cpp \
-    ../cocos2d-x/extensions/GUI/CCControlExtension/CCControl.cpp \
-    ../cocos2d-x/extensions/GUI/CCControlExtension/CCControlButton.cpp \
-    ../cocos2d-x/extensions/GUI/CCControlExtension/CCControlUtils.cpp \
-    ../cocos2d-x/extensions/GUI/CCControlExtension/CCInvocation.cpp \
-    ../cocos2d-x/extensions/GUI/CCEditBox/CCEditBox.cpp \
-    ../cocos2d-x/extensions/GUI/CCEditBox/CCEditBoxImplAndroid.cpp \
-    ../cocos2dx_extensions_luabinding/cocos2dx_extensions_luabinding.cpp
+LOCAL_SRC_FILES := ../cocos2dx_extensions_luabinding/cocos2dx_extensions_luabinding.cpp
 
-LOCAL_EXPORT_C_INCLUDES := $(QUICK_COCOS2DX_ROOT)/lib/cocos2d-x/extensions/ \
-    $(QUICK_COCOS2DX_ROOT)/lib/cocos2d-x/extensions/GUI/CCControlExtension \
-    $(QUICK_COCOS2DX_ROOT)/lib/cocos2d-x/extensions/GUI/CCEditBox \
-    $(QUICK_COCOS2DX_ROOT)/lib/cocos2dx_extensions_luabinding
+LOCAL_EXPORT_C_INCLUDES := $(QUICK_COCOS2DX_ROOT)/lib/cocos2dx_extensions_luabinding
 LOCAL_C_INCLUDES := $(LOCAL_EXPORT_C_INCLUDES)
 
 LOCAL_EXPORT_CFLAGS := -Wno-psabi -DUSE_FILE32API -DCC_LUA_ENGINE_ENABLED=1 -DDEBUG=1 -DCOCOS2D_DEBUG=1
@@ -25,6 +15,7 @@ LOCAL_CFLAGS := -Wno-psabi -DUSE_FILE32API -DCC_LUA_ENGINE_ENABLED=1 -DDEBUG=1 -
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_lua_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_curl_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_extra
@@ -35,6 +26,7 @@ include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,cocos2dx) \
 $(call import-module,CocosDenshion/android) \
+$(call import-module,extensions) \
 $(call import-module,scripting/lua/proj.android) \
 $(call import-module,cocos2dx/platform/third_party/android/prebuilt/libcurl) \
 $(call import-module,lib/cocos2dx_extra/proj.android) \
