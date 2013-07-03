1ce7498186067127c0254c4cfe8f765936c26163
dualface <dualface@gmail.com>
2013-07-03 14:26:13 +0800

    remove unused files

--- sample/CoinFlip/run_simulator_mac.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$DIR"
-"$DIR/../../simulator/bin/mac/LuaHostMac.app/Contents/MacOS/LuaHostMac" -workdir "$DIR" -file scripts/main.lua -size 480x800
