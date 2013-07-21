3930d491fc6e23b9878f33a7e4c646c712b658e5
dualface <dualface@gmail.com>
2013-07-21 10:38:56 +0800

    fix proj.win32 for vs2010

--- bin/update_vs2010_project.sh
+++ bin/update_vs2010_project.sh
@@ -4,10 +4,10 @@ ROOT_DIR=`dirname "$DIR"`
 
 # player/proj.win32
 
-cat $ROOT_DIR/player/proj.win32/quick-x-player_2012.sln | sed 's/Microsoft Visual Studio Solution File, Format Version 12.00/Microsoft Visual Studio Solution File, Format Version 11.00/' | sed 's/Visual Studio 2012/Visual Studio 2010/' | sed 's/_2012/_2010/' > $ROOT_DIR/player/proj.win32/quick-x-player_2010.sln
+cat $ROOT_DIR/player/proj.win32/quick-x-player_2012.sln | sed 's/Microsoft Visual Studio Solution File, Format Version 12.00/Microsoft Visual Studio Solution File, Format Version 11.00/' | sed 's/Visual Studio 2012/Visual Studio 2010/' | sed 's/_2012/_2010/g' > $ROOT_DIR/player/proj.win32/quick-x-player_2010.sln
 
 cp $ROOT_DIR/player/proj.win32/quick-x-player_2012.vcxproj $ROOT_DIR/player/proj.win32/quick-x-player_2010.vcxproj
-cat $ROOT_DIR/player/proj.win32/quick-x-player_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/' > $ROOT_DIR/player/proj.win32/quick-x-player_2010.vcxproj
+cat $ROOT_DIR/player/proj.win32/quick-x-player_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/g' > $ROOT_DIR/player/proj.win32/quick-x-player_2010.vcxproj
 
 cp $ROOT_DIR/player/proj.win32/quick-x-player_2012.vcxproj.filters $ROOT_DIR/player/proj.win32/quick-x-player_2010.vcxproj.filters
 
@@ -15,7 +15,7 @@ cp $ROOT_DIR/player/proj.win32/quick-x-player_2012.vcxproj.filters $ROOT_DIR/pla
 # lib/proj.win32
 
 cp $ROOT_DIR/lib/proj.win32/cocos2dx_2012.vcxproj $ROOT_DIR/lib/proj.win32/cocos2dx_2010.vcxproj
-cat $ROOT_DIR/lib/proj.win32/cocos2dx_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/' > $ROOT_DIR/lib/proj.win32/cocos2dx_2010.vcxproj
+cat $ROOT_DIR/lib/proj.win32/cocos2dx_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/g' > $ROOT_DIR/lib/proj.win32/cocos2dx_2010.vcxproj
 
 cp $ROOT_DIR/lib/proj.win32/cocos2dx_2012.vcxproj.filters $ROOT_DIR/lib/proj.win32/cocos2dx_2010.vcxproj.filters
 
@@ -23,7 +23,7 @@ cp $ROOT_DIR/lib/proj.win32/cocos2dx_2012.vcxproj.filters $ROOT_DIR/lib/proj.win
 # lib/cocos2dx_extra/proj.win32
 
 cp $ROOT_DIR/lib/cocos2dx_extra/proj.win32/cocos2dx_extra_2012.vcxproj $ROOT_DIR/lib/cocos2dx_extra/proj.win32/cocos2dx_extra_2010.vcxproj
-cat $ROOT_DIR/lib/cocos2dx_extra/proj.win32/cocos2dx_extra_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/' > $ROOT_DIR/lib/cocos2dx_extra/proj.win32/cocos2dx_extra_2010.vcxproj
+cat $ROOT_DIR/lib/cocos2dx_extra/proj.win32/cocos2dx_extra_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/g' > $ROOT_DIR/lib/cocos2dx_extra/proj.win32/cocos2dx_extra_2010.vcxproj
 
 cp $ROOT_DIR/lib/cocos2dx_extra/proj.win32/cocos2dx_extra_2012.vcxproj.filters $ROOT_DIR/lib/cocos2dx_extra/proj.win32/cocos2dx_extra_2010.vcxproj.filters
 
@@ -31,7 +31,7 @@ cp $ROOT_DIR/lib/cocos2dx_extra/proj.win32/cocos2dx_extra_2012.vcxproj.filters $
 # lib/lua_extensions/proj.win32
 
 cp $ROOT_DIR/lib/lua_extensions/proj.win32/lua_extensions_2012.vcxproj $ROOT_DIR/lib/lua_extensions/proj.win32/lua_extensions_2010.vcxproj
-cat $ROOT_DIR/lib/lua_extensions/proj.win32/lua_extensions_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/' > $ROOT_DIR/lib/lua_extensions/proj.win32/lua_extensions_2010.vcxproj
+cat $ROOT_DIR/lib/lua_extensions/proj.win32/lua_extensions_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/g' > $ROOT_DIR/lib/lua_extensions/proj.win32/lua_extensions_2010.vcxproj
 
 cp $ROOT_DIR/lib/lua_extensions/proj.win32/lua_extensions_2012.vcxproj.filters $ROOT_DIR/lib/lua_extensions/proj.win32/lua_extensions_2010.vcxproj.filters
 
@@ -39,6 +39,6 @@ cp $ROOT_DIR/lib/lua_extensions/proj.win32/lua_extensions_2012.vcxproj.filters $
 # lib/third_party/proj.win32
 
 cp $ROOT_DIR/lib/third_party/proj.win32/third_party_2012.vcxproj $ROOT_DIR/lib/third_party/proj.win32/third_party_2010.vcxproj
-cat $ROOT_DIR/lib/third_party/proj.win32/third_party_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/' > $ROOT_DIR/lib/third_party/proj.win32/third_party_2010.vcxproj
+cat $ROOT_DIR/lib/third_party/proj.win32/third_party_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012/_2010/g' > $ROOT_DIR/lib/third_party/proj.win32/third_party_2010.vcxproj
 
 cp $ROOT_DIR/lib/third_party/proj.win32/third_party_2012.vcxproj.filters $ROOT_DIR/lib/third_party/proj.win32/third_party_2010.vcxproj.filters
