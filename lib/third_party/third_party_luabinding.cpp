7dbee7b872e1b9ec902cf41e0e0e9353b2b20344
dualface <dualface@gmail.com>
2013-08-27 11:40:16 +0800

    update project

--- lib/third_party/third_party_luabinding.cpp
+++ lib/third_party/third_party_luabinding.cpp
@@ -1,12 +1,13 @@
 
 #include "third_party_luabinding.h"
 #include "chipmunk/luabinding/CCPhysicsWorld_luabinding.h"
-//#include "CSArmature/luabinding/CSArmature_luabinding.h"
+#include "luaproxy/tolua/luaopen_LuaProxy.h"
+
 
 TOLUA_API int luaopen_third_party_luabinding(lua_State* tolua_S)
 {
     luaopen_CCPhysicsWorld_luabinding(tolua_S);
-//    luaopen_CSArmature_luabinding(tolua_S);
+    luaopen_LuaProxy(tolua_S);
 
     return 1;
 };
