5aefba2770b1bd1029bbd60289fdf001aa313800
dualface <dualface@gmail.com>
2013-09-24 01:20:17 +0800

    update template

--- template/PROJECT_TEMPLATE_01/scripts/main.lua
+++ template/PROJECT_TEMPLATE_01/scripts/main.lua
@@ -1,9 +1,9 @@
 
 function __G__TRACKBACK__(errorMessage)
-    CCLuaLog("----------------------------------------")
-    CCLuaLog("LUA ERROR: " .. tostring(errorMessage) .. "\n")
-    CCLuaLog(debug.traceback("", 2))
-    CCLuaLog("----------------------------------------")
+    print("----------------------------------------")
+    print("LUA ERROR: " .. tostring(errorMessage) .. "\n")
+    print(debug.traceback("", 2))
+    print("----------------------------------------")
 end
 
 xpcall(function()
