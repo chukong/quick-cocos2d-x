5eb0ac6d9ac0cb4b9ca49f1b8ee56a5fb3035b6a
dualface <dualface@gmail.com>
2013-09-29 19:19:16 +0800

    add sample statemachine

---
+++ samples/statemachine/scripts/main.lua
@@ -0,0 +1,12 @@
+
+function __G__TRACKBACK__(errorMessage)
+    print("----------------------------------------")
+    print("LUA ERROR: " .. tostring(errorMessage) .. "\n")
+    print(debug.traceback("", 2))
+    print("----------------------------------------")
+end
+
+xpcall(function()
+    require("game")
+    game.startup()
+end, __G__TRACKBACK__)
