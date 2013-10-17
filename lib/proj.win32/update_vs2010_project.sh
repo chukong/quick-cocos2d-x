7fe09821ae5caca129fb8482fc2a5577c407f184
dualface <dualface@gmail.com>
2013-10-17 10:57:08 +0800

    update player win32, remove vs2010 proj

--- lib/proj.win32/update_vs2010_project.sh
+++
@@ -1,6 +0,0 @@
-q#!/bin/sh
-DIR=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
-
-cat $DIR/cocos2dx_2012.vcxproj | sed 's|<PlatformToolset>v110_xp</PlatformToolset>|<PlatformToolset>v100</PlatformToolset>|' | sed 's/_2012\.vcxproj/_2010\.vcxproj/' > $DIR/cocos2dx_2010.vcxproj
-
-cp $DIR/cocos2dx_2012.vcxproj.filters $DIR/cocos2dx_2010.vcxproj.filters
