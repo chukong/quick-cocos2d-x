4129fd814e0fbdb6fca8f91b8eae20c266413306
dualface <dualface@gmail.com>
2013-11-20 19:53:22 +0800

    fix make_framework_package.bat

--- bin/make_framework_package.bat
+++ bin/make_framework_package.bat
@@ -1,6 +1,6 @@
 @echo off
 set DIR=%~dp0
 cd "%DIR%\..\"
-%DIR%win32\php.exe %DIR%\lib\compile_scripts.php -zip -p framework framework lib\framework_precompiled\framework_precompiled
+%DIR%win32\php.exe %DIR%\lib\compile_scripts.php -i framework -o lib\framework_precompiled\framework_precompiled.zip -p framework -m zip
 
 copy lib\framework_precompiled\framework_precompiled.zip template\PROJECT_TEMPLATE_01\res\
