230fc37cdf27f5ca20ae87ca08a1c1495633c03c
yinjimmy <jimmy.yin5@gmail.com>
2014-01-01 17:29:24 +0800

    fix: crash SimpleAudioEngine with Qt

--- lib/cocos2d-x/CocosDenshion/qt/SimpleAudioEngineQt5.cpp
+++ lib/cocos2d-x/CocosDenshion/qt/SimpleAudioEngineQt5.cpp
@@ -53,10 +53,12 @@ class CocosQt5AudioBackend {
         static QMediaPlayer *player() {
             if (background_music == NULL) {
                 background_music = new QMediaPlayer;
+            }
+
+            if (!background_music->playlist()) {
                 QMediaPlaylist *playList = new QMediaPlaylist(background_music);
                 background_music->setPlaylist(playList);
             }
-
             return background_music;
         }
 
@@ -182,10 +184,6 @@ SimpleAudioEngine::playBackgroundMusic(const char* pszFilePath, bool bLoop)
 
 //    CocosQt5AudioBackend::player()->setMedia(QUrl::fromLocalFile(filename));
     QMediaPlaylist *playlist = CocosQt5AudioBackend::player()->playlist();
-    if (!playlist) {
-        playlist = new QMediaPlaylist(CocosQt5AudioBackend::player());
-        CocosQt5AudioBackend::player()->setPlaylist(playlist);
-    }
     playlist->clear();
     playlist->addMedia(QUrl::fromLocalFile(filename));
     if (bLoop) {
