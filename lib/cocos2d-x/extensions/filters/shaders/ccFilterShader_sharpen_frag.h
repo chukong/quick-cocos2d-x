ac9f68f38f4665d09cd64e2a95109c6f6ddc64cd
macmini <macmini@1201.us>
2014-04-08 12:13:31 +0800

    remove precision from shapen filter, because the PVR is not support it

--- lib/cocos2d-x/extensions/filters/shaders/ccFilterShader_sharpen_frag.h
+++ lib/cocos2d-x/extensions/filters/shaders/ccFilterShader_sharpen_frag.h
@@ -1,22 +1,21 @@
-"precision highp float;\n\
-varying highp vec2 v_texCoord;\n\
-varying highp vec2 v_leftTexCoord;\n\
-varying highp vec2 v_rightTexCoord; \n\
-varying highp vec2 v_topTexCoord;\n\
-varying highp vec2 v_bottomTexCoord;\n\
+"varying vec2 v_texCoord;\n\
+varying vec2 v_leftTexCoord;\n\
+varying vec2 v_rightTexCoord; \n\
+varying vec2 v_topTexCoord;\n\
+varying vec2 v_bottomTexCoord;\n\
 \n\
-varying highp float v_centerMultiplier;\n\
-varying highp float v_edgeMultiplier;\n\
+varying float v_centerMultiplier;\n\
+varying float v_edgeMultiplier;\n\
 \n\
 uniform sampler2D CC_Texture0;\n\
 \n\
 void main()\n\
 {\n\
-    mediump vec3 textureColor = texture2D(CC_Texture0, v_texCoord).rgb;\n\
-    mediump vec3 leftTexColor = texture2D(CC_Texture0, v_leftTexCoord).rgb;\n\
-    mediump vec3 rightTexColor = texture2D(CC_Texture0, v_rightTexCoord).rgb;\n\
-    mediump vec3 topTexColor = texture2D(CC_Texture0, v_topTexCoord).rgb;\n\
-    mediump vec3 bottomTexColor = texture2D(CC_Texture0, v_bottomTexCoord).rgb;\n\
+    vec3 textureColor = texture2D(CC_Texture0, v_texCoord).rgb;\n\
+    vec3 leftTexColor = texture2D(CC_Texture0, v_leftTexCoord).rgb;\n\
+    vec3 rightTexColor = texture2D(CC_Texture0, v_rightTexCoord).rgb;\n\
+    vec3 topTexColor = texture2D(CC_Texture0, v_topTexCoord).rgb;\n\
+    vec3 bottomTexColor = texture2D(CC_Texture0, v_bottomTexCoord).rgb;\n\
 \n\
 	vec3 v1 = leftTexColor * v_edgeMultiplier + rightTexColor * v_edgeMultiplier + topTexColor * v_edgeMultiplier + bottomTexColor * v_edgeMultiplier;\n\
 	vec3 v2 = textureColor * v_centerMultiplier - v1;\n\
