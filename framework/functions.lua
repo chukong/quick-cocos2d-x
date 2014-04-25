29f9afd466b798b5e0060deb159231f9713275fb
yuleiliao <yulei.liao@chukong-inc.com>
2014-04-25 17:55:54 +0800

    move

--- framework/functions.lua
+++ framework/functions.lua
@@ -38,8 +38,8 @@ printf("The value = %d", 100)
 
 ~~~
 
-@param fmt 输出格式
-@param ... 更多参数
+@param string fmt 输出格式
+@param [mixed ...] 更多参数
 
 ]]
 function printf(fmt, ...)
@@ -50,69 +50,69 @@ end
 
 检查并尝试转换为数值，如果无法转换则返回 0
 
-@param v 要检查的值
-@param base 进制，默认为十进制
+@param mixed value 要检查的值
+@param [integer base] 进制，默认为十进制
 
-@return 结果
+@return number
 
 ]]
-function checknumber(v, base)
-    return tonumber(v, base) or 0
+function checknumber(value, base)
+    return tonumber(value, base) or 0
 end
 
 --[[--
 
 检查并尝试转换为整数，如果无法转换则返回 0
 
-@param v 要检查的值
+@param mixed value 要检查的值
 
-@return 结果
+@return integer
 
 ]]
-function checkint(v)
-    return math.round(checknumber(v))
+function checkint(value)
+    return math.round(checknumber(value))
 end
 
 --[[--
 
 检查并尝试转换为布尔值，除了 nil 和 false，其他任何值都会返回 true
 
-@param v 要检查的值
+@param mixed value 要检查的值
 
-@return 结果
+@return boolean
 
 ]]
-function checkbool(v)
-    return (v ~= nil and v ~= false)
+function checkbool(value)
+    return (value ~= nil and value ~= false)
 end
 
 --[[--
 
 检查值是否是一个表格，如果不是则返回一个空表格
 
-@param v 要检查的值
+@param mixed value 要检查的值
 
-@return 结果
+@return table
 
 ]]
-function checktable(v)
-    if type(v) ~= "table" then v = {} end
-    return v
+function checktable(value)
+    if type(value) ~= "table" then value = {} end
+    return value
 end
 
 --[[--
 
 如果表格中指定 key 的值为 nil，或者输入值不是表格，返回 false，否则返回 true
 
-@param arr 要检查的表格
-@param key 要检查的键名
+@param table hashtable 要检查的表格
+@param mixed key 要检查的键名
 
-@return 结果
+@return boolean
 
 ]]
-function isset(arr, key)
-    local t = type(arr)
-    return (t == "table" or t == "userdata") and arr[key] ~= nil
+function isset(hashtable, key)
+    local t = type(hashtable)
+    return (t == "table" or t == "userdata") and hashtable[key] ~= nil
 end
 
 --[[--
@@ -133,9 +133,9 @@ t2.b = 3    -- t1 = {a = 1, b = 2} <-- t1.b 不受影响
 
 ~~~
 
-@param object 要克隆的值
+@param mixed object 要克隆的值
 
-@return 结果
+@return mixed
 
 ]]
 function clone(object)
@@ -274,10 +274,10 @@ end
 
 **注意:** Lua 继承类覆盖的方法并不能从 C++ 调用到。也就是说通过 C++ 代码调用这个 CCNode 对象的 setPosition() 方法时，并不会执行我们在 Lua 中定义的 Toolbar:setPosition() 方法。
 
-@param classname 类名
-@param super 父类或者创建对象实例的函数
+@param string classname 类名
+@param [mixed super] 父类或者创建对象实例的函数
 
-@return 类定义
+@return table
 
 ]]
 function class(classname, super)
@@ -353,10 +353,10 @@ print(iskindof(Duck.new(), "Animal")) -- 输出 true
 
 ~~~
 
-@param obj 要检查的对象
-@param classname 类名
+@param mixed obj 要检查的对象
+@param string classname 类名
 
-@return 结果
+@return boolean
 
 ]]
 function iskindof(obj, classname)
@@ -424,9 +424,9 @@ local Data2 = import(".data.Data2")
 
 ~~~
 
-当在模块名前面有一个“.”时，import() 会从当前模块所在目录中查找其他模块。因此 MyClass 及其相关文件不管存放到什么目录里，我们都不再需要修改 MyClass 中的 import() 命令。这在开发一些重复使用的功能组件时，会非常方便。
+当在模块名前面有一个"." 时，import() 会从当前模块所在目录中查找其他模块。因此 MyClass 及其相关文件不管存放到什么目录里，我们都不再需要修改 MyClass 中的 import() 命令。这在开发一些重复使用的功能组件时，会非常方便。
 
-我们可以在模块名前添加多个“.”，这样 import() 会从更上层的目录开始查找模块。
+我们可以在模块名前添加多个"." ，这样 import() 会从更上层的目录开始查找模块。
 
 ~
 
@@ -446,10 +446,10 @@ end
 
 ~~~
 
-@param moduleName 要载入的模块的名字
-@param currentModuleName 当前模块名
+@param string moduleName 要载入的模块的名字
+@param [string currentModuleName] 当前模块名
 
-@return 返回载入的模块的返回值
+@return module
 
 ]]
 function import(moduleName, currentModuleName)
@@ -505,7 +505,7 @@ end
 
 ~~~
 
-上述代码执行时将出错，报告“Invalid self”，这就是因为 C++ 无法识别 Lua 对象方法。因此在调用我们传入的 self.onEnterFrame 方法时没有提供正确的参数。
+上述代码执行时将出错，报告"Invalid self" ，这就是因为 C++ 无法识别 Lua 对象方法。因此在调用我们传入的 self.onEnterFrame 方法时没有提供正确的参数。
 
 要让上述的代码正常工作，就需要使用 handler() 进行一下包装：
 
@@ -521,19 +521,24 @@ end
 
 实际上，除了 C++ 回调 Lua 函数之外，在其他所有需要回调的地方都可以使用 handler()。
 
-@param target Lua 对象
-@param method 对象方法
+@param mixed obj Lua 对象
+@param function method 对象方法
 
-@return 包装后的匿名函数
+@return function
 
 ]]
-function handler(target, method)
+function handler(obj, method)
     return function(...)
-        return method(target, ...)
+        return method(obj, ...)
     end
 end
 
-function math.newrendomseed()
+--[[--
+
+根据系统时间初始化随机数种子，让后续的 math.random() 返回更随机的值
+
+]]
+function math.newrandomseed()
     local ok, socket = pcall(function()
         return require("socket")
     end)
@@ -550,10 +555,39 @@ function math.newrendomseed()
     math.random()
 end
 
-function math.round(num)
-    return math.floor(num + 0.5)
+--[[--
+
+对数值进行四舍五入，如果不是数值则返回 0
+
+@param number value 输入值
+
+@return number
+
+]]
+function math.round(value)
+    return math.floor(value + 0.5)
 end
 
+--[[--
+
+检查指定的文件或目录是否存在，如果存在返回 true，否则返回 false
+
+可以使用 CCFileUtils:fullPathForFilename() 函数查找特定文件的完整路径，例如：
+
+~~~ lua
+
+local path = CCFileUtils:sharedFileUtils():fullPathForFilename("gamedata.txt")
+if io.exists(path) then
+    ....
+end
+
+~~~
+
+@param string path 要检查的文件或目录的完全路径
+
+@return boolean
+
+]]
 function io.exists(path)
     local file = io.open(path, "r")
     if file then
@@ -563,6 +597,17 @@ function io.exists(path)
     return false
 end
 
+--[[--
+
+读取文件内容，返回包含文件内容的字符串，如果失败返回 nil
+
+io.readfile() 会一次性读取整个文件的内容，并返回一个字符串，因此该函数不适宜读取太大的文件。
+
+@param string path 文件完全路径
+
+@return string
+
+]]
 function io.readfile(path)
     local file = io.open(path, "r")
     if file then
@@ -573,6 +618,26 @@ function io.readfile(path)
     return nil
 end
 
+--[[--
+
+以字符串内容写入文件，成功返回 true，失败返回 false
+
+"mode 写入模式" 参数决定 io.writefile() 如何写入内容，可用的值如下：
+
+-   "w+" : 覆盖文件已有内容，如果文件不存在则创建新文件
+-   "a+" : 追加内容到文件尾部，如果文件不存在则创建文件
+
+此外，还可以在 "写入模式" 参数最后追加字符 "b" ，表示以二进制方式写入数据，这样可以避免内容写入不完整。
+
+**Android 特别提示:** 在 Android 平台上，文件只能写入存储卡所在路径，assets 和 data 等目录都是无法写入的。
+
+@param string path 文件完全路径
+@param string content 要写入的内容
+@param [string mode] 写入模式，默认值为 "w+b"
+
+@return boolean
+
+]]
 function io.writefile(path, content, mode)
     mode = mode or "w+b"
     local file = io.open(path, mode)
@@ -585,6 +650,27 @@ function io.writefile(path, content, mode)
     end
 end
 
+--[[--
+
+拆分一个路径字符串，返回组成路径的各个部分
+
+~~~ lua
+
+local pathinfo  = io.pathinfo("/var/app/test/abc.png")
+
+-- 结果:
+-- pathinfo.dirname  = "/var/app/test/"
+-- pathinfo.filename = "abc.png"
+-- pathinfo.basename = "abc"
+-- pathinfo.extname  = ".png"
+
+~~~
+
+@param string path 要分拆的路径字符串
+
+@return table
+
+]]
 function io.pathinfo(path)
     local pos = string.len(path)
     local extpos = pos + 1
@@ -611,6 +697,15 @@ function io.pathinfo(path)
     }
 end
 
+--[[--
+
+返回指定文件的大小，如果失败返回 false
+
+@param string path 文件完全路径
+
+@return integer
+
+]]
 function io.filesize(path)
     local size = false
     local file = io.open(path, "r")
@@ -623,6 +718,17 @@ function io.filesize(path)
     return size
 end
 
+--[[--
+
+计算表格包含的字段数量
+
+Lua table 的 "#" 操作只对依次排序的数值下标数组有效，table.nums() 则计算 table 中所有不为 nil 的值的个数。
+
+@param table t 要检查的表格
+
+@return integer
+
+]]
 function table.nums(t)
     local count = 0
     for k, v in pairs(t) do
@@ -631,22 +737,73 @@ function table.nums(t)
     return count
 end
 
-function table.keys(t)
+--[[--
+
+返回指定表格中的所有键
+
+~~~ lua
+
+local hashtable = {a = 1, b = 2, c = 3}
+local keys = table.keys(hashtable)
+-- keys = {"a", "b", "c"}
+
+~~~
+
+@param table hashtable 要检查的表格
+
+@return table
+
+]]
+function table.keys(hashtable)
     local keys = {}
-    for k, v in pairs(t) do
+    for k, v in pairs(hashtable) do
         keys[#keys + 1] = k
     end
     return keys
 end
 
-function table.values(t)
+--[[--
+
+返回指定表格中的所有值
+
+~~~ lua
+
+local hashtable = {a = 1, b = 2, c = 3}
+local values = table.values(hashtable)
+-- values = {1, 2, 3}
+
+~~~
+
+@param table hashtable 要检查的表格
+
+@return table
+
+]]
+function table.values(hashtable)
     local values = {}
-    for k, v in pairs(t) do
+    for k, v in pairs(hashtable) do
         values[#values + 1] = v
     end
     return values
 end
 
+--[[--
+
+将来源表格中所有键及其值复制到目标表格对象中，如果存在同名键，则覆盖其值
+
+~~~ lua
+
+local dest = {a = 1, b = 2}
+local src  = {c = 3, d = 4}
+table.merge(dest, src)
+-- dest = {a = 1, b = 2, c = 3, d = 4}
+
+~~~
+
+@param table dest 目标表格
+@param table src 来源表格
+
+]]
 function table.merge(dest, src)
     for k, v in pairs(src) do
         dest[k] = v
@@ -655,26 +812,29 @@ end
 
 --[[--
 
-insert list.
+在目标表格的指定位置插入来源表格，如果没有指定位置则连接两个表格
 
-**Usage:**
+~~~ lua
 
-    local dest = {1, 2, 3}
-    local src  = {4, 5, 6}
-    table.insertTo(dest, src)
-    -- dest = {1, 2, 3, 4, 5, 6}
-	dest = {1, 2, 3}
-	table.insertTo(dest, src, 5)
-    -- dest = {1, 2, 3, nil, 4, 5, 6}
+local dest = {1, 2, 3}
+local src  = {4, 5, 6}
+table.insertto(dest, src)
+-- dest = {1, 2, 3, 4, 5, 6}
 
+dest = {1, 2, 3}
+table.insertto(dest, src, 5)
+-- dest = {1, 2, 3, nil, 4, 5, 6}
 
-@param table dest
-@param table src
-@param table begin insert position for dest
+~~~
+
+@param table dest 目标表格
+@param table src 来源表格
+@param [integer begin] 插入位置
+
 ]]
-function table.insertTo(dest, src, begin)
-	begin = tonumber(begin)
-	if begin == nil then
+function table.insertto(dest, src, begin)
+	begin = checkint(begin)
+	if begin <= 0 then
 		begin = #dest + 1
 	end
 
@@ -685,101 +845,243 @@ function table.insertTo(dest, src, begin)
 end
 
 --[[
-search target index at list.
 
-@param table list
-@param * target
-@param int from idx, default 1
-@param bool useNaxN, the len use table.maxn(true) or #(false) default:false
-@param return index of target at list, if not return -1
+从表格中查找指定值，返回其索引，如果没找到返回 false
+
+~~~ lua
+
+local array = {"a", "b", "c"}
+print(table.indexof(array, "b")) -- 输出 2
+
+~~~
+
+@param table array 表格
+@param mixed value 要查找的值
+@param [integer begin] 起始索引值
+
+@return integer
+
 ]]
-function table.indexOf(list, target, from, useMaxN)
-	local len = (useMaxN and #list) or table.maxn(list)
-	if from == nil then
-		from = 1
-	end
-	for i = from, len do
-		if list[i] == target then
-			return i
-		end
-	end
-	return -1
+function table.indexof(array, value, begin)
+    for i = begin or 1, #array do
+        if array[i] == value then return i end
+    end
+	return false
 end
 
-function table.indexOfKey(list, key, value, from, useMaxN)
-	local len = (useMaxN and #list) or table.maxn(list)
-	if from == nil then
-		from = 1
-	end
-	local item = nil
-	for i = from, len do
-		item = list[i]
-		if item ~= nil and item[key] == value then
-			return i
-		end
-	end
-	return -1
+--[[--
+
+从表格中查找指定值，返回其 key，如果没找到返回 nil
+
+~~~ lua
+
+local hashtable = {name = "dualface", comp = "chukong"}
+print(table.keyof(hashtable, "chukong")) -- 输出 comp
+
+~~~
+
+@param table hashtable 表格
+@param mixed value 要查找的值
+
+@return string 该值对应的 key
+
+]]
+function table.keyof(hashtable, value)
+    for k, v in pairs(hashtable) do
+        if v == value then return k end
+    end
+    return nil
 end
 
-function table.removeItem(t, item, removeAll)
-    for i = #t, 1, -1 do
-        if t[i] == item then
-            table.remove(t, i)
-            if not removeAll then break end
+--[[--
+
+从表格中删除指定值，返回删除的值的个数
+
+~~~ lua
+
+local array = {"a", "b", "c", "c"}
+print(table.removebyvalue(array, "c", true)) -- 输出 2
+
+~~~
+
+@param table array 表格
+@param mixed value 要删除的值
+@param [boolean removeall] 是否删除所有相同的值
+
+@return integer
+
+]]
+function table.removebyvalue(array, value, removeall)
+    local c, i, max = 0, 1, #array
+    while i <= max do
+        if array[i] == value then
+            table.remove(array, i)
+            c = c + 1
+            i = i - 1
+            max = max - 1
+            if not removeall then break end
         end
+        i = i + 1
+    end
+    return c
+end
+
+--[[--
+
+对表格中每一个值执行一次指定的函数，并用函数返回值更新表格内容
+
+~~~ lua
+
+local t = {name = "dualface", comp = "chukong"}
+table.map(t, function(v, k)
+    -- 在每一个值前后添加括号
+    return "[" .. v .. "]"
+end)
+
+-- 输出修改后的表格内容
+for k, v in pairs(t) do
+    print(k, v)
+end
+
+-- 输出
+-- name [dualface]
+-- comp [chukong]
+
+~~~
+
+fn 参数指定的函数具有两个参数，并且返回一个值。原型如下：
+
+~~~ lua
+
+function map_function(value, key)
+    return value
+end
+
+~~~
+
+@param table t 表格
+@param function fn 函数
+
+]]
+function table.map(t, fn)
+    for k, v in pairs(t) do
+        t[k] = fn(v, k)
     end
 end
 
-function table.map(t, fun)
-    for k,v in pairs(t) do
-        t[k] = fun(v, k)
-    end
+--[[--
+
+对表格中每一个值执行一次指定的函数，但不改变表格内容
+
+~~~ lua
+
+local t = {name = "dualface", comp = "chukong"}
+table.walk(t, function(v, k)
+    -- 输出每一个值
+    print(v)
+end)
+
+~~~
+
+fn 参数指定的函数具有两个参数，没有返回值。原型如下：
+
+~~~ lua
+
+function map_function(value, key)
+
 end
 
-function table.walk(t, fun)
+~~~
+
+@param table t 表格
+@param function fn 函数
+
+]]
+
+function table.walk(t, fn)
     for k,v in pairs(t) do
         fun(v, k)
     end
 end
 
-function table.filter(t, fun)
-    for k,v in pairs(t) do
-        if not fun(v, k) then
-            t[k] = nil
-        end
+--[[--
+
+对表格中每一个值执行一次指定的函数，如果该函数返回 false，则对应的值会从表格中删除
+
+~~~ lua
+
+local t = {name = "dualface", comp = "chukong"}
+table.filter(t, function(v, k)
+    return v ~= "dualface" -- 当值等于 dualface 时过滤掉该值
+end)
+
+-- 输出修改后的表格内容
+for k, v in pairs(t) do
+    print(k, v)
+end
+
+-- 输出
+-- comp chukong
+
+~~~
+
+fn 参数指定的函数具有两个参数，并且返回一个 boolean 值。原型如下：
+
+~~~ lua
+
+function map_function(value, key)
+    return true or false
+end
+
+~~~
+
+@param table t 表格
+@param function fn 函数
+
+]]
+function table.filter(t, fn)
+    for k, v in pairs(t) do
+        if not fn(v, k) then t[k] = nil end
     end
 end
 
-function table.find(t, item)
-    return table.keyOfItem(t, item) ~= nil
+--[[--
+
+遍历表格，确保其中的值唯一
+
+~~~ lua
+
+local t = {"a", "a", "b", "c"} -- 重复的 a 会被过滤掉
+local n = table.unique(t)
+
+for k, v in pairs(n) do
+    print(v)
 end
 
+-- 输出
+-- a
+-- b
+-- c
+
+~~~
+
+@param table t 表格
+
+@return table 包含所有唯一值的新表格
+
+]]
 function table.unique(t)
-    local r = {}
+    local check = {}
     local n = {}
-    for i = #t, 1, -1 do
-        local v = t[i]
-        if not r[v] then
-            r[v] = true
-            n[#n + 1] = v
+    for k, v in pairs(t) do
+        if not check[v] then
+            n[k] = v
+            check[v] = true
         end
     end
     return n
 end
 
-function table.keyOfItem(t, item)
-    for k,v in pairs(t) do
-        if v == item then return k end
-    end
-    return nil
-end
-
-function string.htmlspecialchars(input)
-    for k, v in pairs(string._htmlspecialchars_set) do
-        input = string.gsub(input, k, v)
-    end
-    return input
-end
 string._htmlspecialchars_set = {}
 string._htmlspecialchars_set["&"] = "&amp;"
 string._htmlspecialchars_set["\""] = "&quot;"
@@ -787,13 +1089,69 @@ string._htmlspecialchars_set["'"] = "&#039;"
 string._htmlspecialchars_set["<"] = "&lt;"
 string._htmlspecialchars_set[">"] = "&gt;"
 
-function string.htmlspecialcharsDecode(input)
+--[[--
+
+将特殊字符转为 HTML 标示
+
+~~~ lua
+
+print(string.htmlspecialchars("<ABC>"))
+-- 输出 &lt;ABC&gt;
+
+~~~
+
+@param string input 输入字符串
+
+@return string 转换结果
+
+]]
+function string.htmlspecialchars(input)
+    for k, v in pairs(string._htmlspecialchars_set) do
+        input = string.gsub(input, k, v)
+    end
+    return input
+end
+
+--[[--
+
+将 HTML 标示还原为特殊字符，功能与 string.htmlspecialchars() 正好相反
+
+~~~ lua
+
+print(string.restorehtmlspecialchars("&lt;ABC&gt;"))
+-- 输出 <ABC>
+
+~~~
+
+@param string input 输入字符串
+
+@return string 转换结果
+
+]]
+function string.restorehtmlspecialchars(input)
     for k, v in pairs(string._htmlspecialchars_set) do
         input = string.gsub(input, v, k)
     end
     return input
 end
 
+--[[--
+
+将字符串中的 \n 换行符转换为 HTML 标记 <br />
+
+~~~ lua
+
+print(string.nl2br("Hello\nWorld"))
+-- 输出
+-- Hello<br />World
+
+~~~
+
+@param string input 输入字符串
+
+@return string 转换结果
+
+]]
 function string.nl2br(input)
     return string.gsub(input, "\n", "<br />")
 end
