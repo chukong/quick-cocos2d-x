70b01e883a00f554f0e1927f4dc87c6f64fa80fb
dualface <dualface@gmail.com>
2014-04-26 20:19:46 +0800

    move

--- framework/functions.lua
+++ framework/functions.lua
@@ -1091,7 +1091,7 @@ string._htmlspecialchars_set[">"] = "&gt;"
 
 --[[--
 
-将特殊字符转为 HTML 标示
+将特殊字符转为 HTML 转义符
 
 ~~~ lua
 
@@ -1114,7 +1114,7 @@ end
 
 --[[--
 
-将 HTML 标示还原为特殊字符，功能与 string.htmlspecialchars() 正好相反
+将 HTML 转义符还原为特殊字符，功能与 string.htmlspecialchars() 正好相反
 
 ~~~ lua
 
@@ -1137,7 +1137,7 @@ end
 
 --[[--
 
-将字符串中的 \n 换行符转换为 HTML 标记 <br />
+将字符串中的 \n 换行符转换为 HTML 标记
 
 ~~~ lua
 
@@ -1156,6 +1156,23 @@ function string.nl2br(input)
     return string.gsub(input, "\n", "<br />")
 end
 
+--[[--
+
+将字符串中的特殊字符和 \n 换行符转换为 HTML 转移符和标记
+
+~~~ lua
+
+print(string.nl2br("<Hello>\nWorld"))
+-- 输出
+-- &lt;Hello&gt;<br />World
+
+~~~
+
+@param string input 输入字符串
+
+@return string 转换结果
+
+]]
 function string.text2html(input)
     input = string.gsub(input, "\t", "    ")
     input = string.htmlspecialchars(input)
@@ -1164,64 +1181,216 @@ function string.text2html(input)
     return input
 end
 
-function string.split(str, delimiter)
-    str = tostring(str)
+--[[--
+
+用指定字符或字符串分割输入字符串，返回包含分割结果的数组
+
+~~~ lua
+
+local input = "Hello,World"
+local res = string.split(input, ",")
+-- res = {"Hello", "World"}
+
+local input = "Hello-+-World-+-Quick"
+local res = string.split(input, "-+-")
+-- res = {"Hello", "World", "Quick"}
+
+~~~
+
+@param string input 输入字符串
+@param string delimiter 分割标记字符或字符串
+
+@return array 包含分割结果的数组
+
+]]
+function string.split(input, delimiter)
+    input = tostring(input)
     delimiter = tostring(delimiter)
     if (delimiter=='') then return false end
     local pos,arr = 0, {}
     -- for each divider found
-    for st,sp in function() return string.find(str, delimiter, pos, true) end do
-        table.insert(arr, string.sub(str, pos, st - 1))
+    for st,sp in function() return string.find(input, delimiter, pos, true) end do
+        table.insert(arr, string.sub(input, pos, st - 1))
         pos = sp + 1
     end
-    table.insert(arr, string.sub(str, pos))
+    table.insert(arr, string.sub(input, pos))
     return arr
 end
 
-function string.ltrim(str)
-    return string.gsub(str, "^[ \t\n\r]+", "")
+--[[--
+
+去除输入字符串头部的空白字符，返回结果
+
+~~~ lua
+
+local input = "  ABC"
+print(string.ltrim(input))
+-- 输出 ABC，输入字符串前面的两个空格被去掉了
+
+~~~
+
+空白字符包括：
+
+-   空格
+-   制表符 \t
+-   换行符 \n
+-   回到行首符 \r
+
+@param string input 输入字符串
+
+@return string 结果
+
+@see string.rtrim, string.trim
+
+]]
+function string.ltrim(input)
+    return string.gsub(input, "^[ \t\n\r]+", "")
 end
 
-function string.rtrim(str)
-    return string.gsub(str, "[ \t\n\r]+$", "")
+--[[--
+
+去除输入字符串尾部的空白字符，返回结果
+
+~~~ lua
+
+local input = "ABC  "
+print(string.ltrim(input))
+-- 输出 ABC，输入字符串最后的两个空格被去掉了
+
+~~~
+
+@param string input 输入字符串
+
+@return string 结果
+
+@see string.ltrim, string.trim
+
+]]
+function string.rtrim(input)
+    return string.gsub(input, "[ \t\n\r]+$", "")
 end
 
-function string.trim(str)
-    str = string.gsub(str, "^[ \t\n\r]+", "")
-    return string.gsub(str, "[ \t\n\r]+$", "")
+--[[--
+
+去掉字符串首尾的空白字符，返回结果
+
+@param string input 输入字符串
+
+@return string 结果
+
+@see string.ltrim, string.rtrim
+
+]]
+function string.trim(input)
+    input = string.gsub(input, "^[ \t\n\r]+", "")
+    return string.gsub(input, "[ \t\n\r]+$", "")
 end
 
-function string.ucfirst(str)
-    return string.upper(string.sub(str, 1, 1)) .. string.sub(str, 2)
+--[[--
+
+将字符串的第一个字符转为大写，返回结果
+
+~~~ lua
+
+local input = "hello"
+print(string.ucfirst(input))
+-- 输出 Hello
+
+~~~
+
+@param string input 输入字符串
+
+@return string 结果
+
+]]
+function string.ucfirst(input)
+    return string.upper(string.sub(input, 1, 1)) .. string.sub(input, 2)
 end
 
-local function urlencodeChar(char)
+local function urlencodechar(char)
     return "%" .. string.format("%02X", string.byte(c))
 end
 
-function string.urlencode(str)
+--[[--
+
+将字符串转换为符合 URL 传递要求的格式，并返回转换结果
+
+~~~ lua
+
+local input = "hello world"
+print(string.urlencode(input))
+-- 输出
+-- hello%20world
+
+~~~
+
+@param string input 输入字符串
+
+@return string 转换后的结果
+
+@see string.urldecode
+
+]]
+function string.urlencode(input)
     -- convert line endings
-    str = string.gsub(tostring(str), "\n", "\r\n")
+    input = string.gsub(tostring(input), "\n", "\r\n")
     -- escape all characters but alphanumeric, '.' and '-'
-    str = string.gsub(str, "([^%w%.%- ])", urlencodeChar)
+    input = string.gsub(input, "([^%w%.%- ])", urlencodechar)
     -- convert spaces to "+" symbols
-    return string.gsub(str, " ", "+")
+    return string.gsub(input, " ", "+")
 end
 
-function string.urldecode(str)
-    str = string.gsub (str, "+", " ")
-    str = string.gsub (str, "%%(%x%x)", function(h) return string.char(checknumber(h,16)) end)
-    str = string.gsub (str, "\r\n", "\n")
-    return str
+--[[--
+
+将 URL 中的特殊字符还原，并返回结果
+
+~~~ lua
+
+local input = "hello%20world"
+print(string.urldecode(input))
+-- 输出
+-- hello world
+
+~~~
+
+@param string input 输入字符串
+
+@return string 转换后的结果
+
+@see string.urlencode
+
+]]
+function string.urldecode(input)
+    input = string.gsub (input, "+", " ")
+    input = string.gsub (input, "%%(%x%x)", function(h) return string.char(checknumber(h,16)) end)
+    input = string.gsub (input, "\r\n", "\n")
+    return input
 end
 
-function string.utf8len(str)
-    local len  = #str
+--[[--
+
+计算 UTF8 字符串的长度，每一个中文算一个字符
+
+~~~ lua
+
+local input = "你好World"
+print(string.utf8len(input))
+-- 输出 7
+
+~~~
+
+@param string input 输入字符串
+
+@return integer 长度
+
+]]
+function string.utf8len(input)
+    local len  = string.len(input)
     local left = len
     local cnt  = 0
     local arr  = {0, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc}
     while left ~= 0 do
-        local tmp = string.byte(str, -left)
+        local tmp = string.byte(input, -left)
         local i   = #arr
         while arr[i] do
             if tmp >= arr[i] then
@@ -1233,14 +1402,4 @@ function string.utf8len(str)
         cnt = cnt + 1
     end
     return cnt
-end
-
-function string.formatNumberThousands(num)
-    local formatted = tostring(checknumber(num))
-    local k
-    while true do
-        formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
-        if k == 0 then break end
-    end
-    return formatted
 end
