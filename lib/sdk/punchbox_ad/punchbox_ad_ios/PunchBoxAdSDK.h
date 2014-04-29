5e14f381bcbef12b2c15f937b77ac6afc246a06f
htl <htlxyz@126.com>
2014-04-29 18:41:43 +0800

    add integration add IOS code

--- lib/sdk/punchbox_ad/punchbox_ad_ios/PunchBoxAdSDK.h
+++ lib/sdk/punchbox_ad/punchbox_ad_ios/PunchBoxAdSDK.h
@@ -2,17 +2,18 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@interface PunchBoxAdSDK
+@interface PunchBoxAdSDK : NSObject
 
 + (PunchBoxAdSDK*) getInstance;
-
 
 // public interface
 
 + (void) start:(NSDictionary*)options;
-+ (void) stop:(NSDictionary*)options;
-+ (void)
-
++ (void) stop;
++ (void) show:(NSDictionary*)options;
++ (void) remove;
++ (void) addScriptListener:(NSDictionary*)options;
++ (void) removeScriptListener;
 
 @end
 
--- lib/sdk/punchbox_ad/punchbox_ad_ios/PunchBoxAdSDK.mm
+++ lib/sdk/punchbox_ad/punchbox_ad_ios/PunchBoxAdSDK.mm
@@ -1,2 +1,54 @@
 
 #include "PunchBoxAdSDK.h"
+#include "CCLuaBridge.h"
+
+static PunchBoxAdSDK *gPunchBoxAdSDKInstance = nil;
+static NSString *appId = nil;
+static int functionId = 0;
+
+@implementation PunchBoxAdSDK
+
+
++ (PunchBoxAdSDK*) getInstance
+{
+    @synchronized(self) {
+        if (gPunchBoxAdSDKInstance == nil) {
+            gPunchBoxAdSDKInstance = [[PunchBoxAdSDK alloc] init];
+        }
+    }
+    
+    return gPunchBoxAdSDKInstance;
+}
+
++ (void) start:(NSDictionary*)options
+{
+    appId = [options objectForKey:@"appId"];
+}
+
++ (void) stop
+{
+    removeScriptListener();
+}
+
++ (void) show:(NSDictionary*)options
+{
+    
+}
+
++ (void) remove
+{
+    
+}
+
++ (void) addScriptListener:(NSDictionary*)options
+{
+    NSNumber *number = [options objectForKey:@"listener"];
+    functionId = [number intValue];
+}
+
++ (void) removeScriptListener
+{
+    cocos2d::CCLuaBridge::releaseLuaFunctionById(functionId);
+}
+
+@end
