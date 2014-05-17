f2363e5ad920ba723dbe6958515a7f5938591f08
yinjimmy <jimmy.yin5@gmail.com>
2014-05-17 19:19:28 +0800

    missing samples.lua

---
+++ samples/samples.lua
@@ -0,0 +1,102 @@
+
+local data =
+{
+    {
+        title       = "2048",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/2048",
+    },
+
+    {
+        title       = "benchmark",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/benchmark",
+    },
+
+    {
+        title       = "cocos2dx_luatest",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/mvc",
+    },
+
+    {
+        title       = "coinflip",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/coinflip",
+    },
+
+    {
+        title       = "dragonbones",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/dragonbones",
+    },
+
+    {
+        title       = "editbox",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/editbox",
+    },
+
+    {
+        title       = "filters",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/filters",
+    },
+
+    {
+        title       = "luasocket",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/luasocket",
+    },
+
+    {
+        title       = "mvc",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/mvc",
+    },
+
+    {
+        title       = "nightclub",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/nightclub",
+    },
+
+    {
+        title       = "physics",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/physics",
+    },
+
+    {
+        title       = "tests",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/tests",
+    },
+
+    {
+        title       = "statemachine",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/statemachine",
+    },
+
+    {
+        title       = "touchsprite",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/touchsprite",
+    },
+
+    {
+        title       = "ui",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/ui",
+    },
+
+    {
+        title       = "websockets",
+        description = "演示如何创建可触摸的对象",
+        path        = "samples/websockets",
+    },
+
+}
+
+return data
