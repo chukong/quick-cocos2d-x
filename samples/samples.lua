9da8546249942563420a05fbc0d47ae989c2558e
dualface <dualface@gmail.com>
2014-06-02 16:43:26 +0800

    update samples titles

--- samples/samples.lua
+++ samples/samples.lua
@@ -3,97 +3,145 @@ local data =
 {
     {
         title       = "2048",
-        description = "演示如何创建可触摸的对象",
+        description = "用 quick 开发的 2048 游戏",
         path        = "samples/2048",
     },
 
     {
         title       = "benchmark",
-        description = "演示如何创建可触摸的对象",
+        description = "性能测试（推荐真机运行）",
         path        = "samples/benchmark",
     },
 
     {
-        title       = "cocos2dx_luatest",
-        description = "演示如何创建可触摸的对象",
-        path        = "samples/mvc",
+        title       = "chukong_ad",
+        description = "触控广告 SDK 集成（真机）",
+        path        = "samples/chukong_ad",
+    },
+
+    {
+        title       = "cocopush",
+        description = "触控 Push SDK 集成（真机）",
+        path        = "samples/cocopush",
     },
 
     {
         title       = "coinflip",
-        description = "演示如何创建可触摸的对象",
+        description = "一个翻转金币的小游戏",
         path        = "samples/coinflip",
     },
 
     {
         title       = "dragonbones",
-        description = "演示如何创建可触摸的对象",
+        description = "DragonBones 骨骼动画",
         path        = "samples/dragonbones",
     },
 
     {
         title       = "editbox",
-        description = "演示如何创建可触摸的对象",
+        description = "编辑框示例",
         path        = "samples/editbox",
     },
 
     {
         title       = "filters",
-        description = "演示如何创建可触摸的对象",
+        description = "Sprite 过滤器特效示例",
         path        = "samples/filters",
     },
 
     {
+        title       = "iap4ios",
+        description = "iOS IAP 使用（真机）",
+        path        = "samples/iap4ios",
+    },
+
+    {
+        title       = "luajavabridge",
+        description = "Lua Java 互操作（真机）",
+        path        = "samples/luajavabridge",
+    },
+
+    {
         title       = "luasocket",
-        description = "演示如何创建可触摸的对象",
+        description = "Socket 网络",
         path        = "samples/luasocket",
     },
 
     {
         title       = "mvc",
-        description = "演示如何创建可触摸的对象",
+        description = "MVC 模式",
         path        = "samples/mvc",
     },
 
     {
         title       = "nightclub",
-        description = "演示如何创建可触摸的对象",
+        description = "一个游戏 demo",
         path        = "samples/nightclub",
     },
 
     {
         title       = "physics",
-        description = "演示如何创建可触摸的对象",
+        description = "物理引擎演示",
         path        = "samples/physics",
     },
 
     {
-        title       = "tests",
-        description = "演示如何创建可触摸的对象",
-        path        = "samples/tests",
-    },
-
-    {
         title       = "statemachine",
-        description = "演示如何创建可触摸的对象",
+        description = "状态机使用",
         path        = "samples/statemachine",
     },
 
     {
-        title       = "touchsprite",
-        description = "演示如何创建可触摸的对象",
-        path        = "samples/touchsprite",
+        title       = "tests",
+        description = "测试",
+        path        = "samples/tests",
+    },
+
+    {
+        title       = "touch",
+        description = "触摸机制测试",
+        path        = "samples/touch",
     },
 
     {
         title       = "ui",
-        description = "演示如何创建可触摸的对象",
+        description = "UI 系统测试",
         path        = "samples/ui",
     },
 
     {
+        title       = "umeng_analytics",
+        description = "友盟统计 SDK（真机）",
+        path        = "samples/umeng_analytics",
+    },
+
+    {
+        title       = "umeng_feedback",
+        description = "友盟反馈 SDK（真机）",
+        path        = "samples/umeng_feedback",
+    },
+
+    {
+        title       = "umeng_push",
+        description = "友盟 Push SDK（真机）",
+        path        = "samples/umeng_push",
+    },
+
+    {
+        title       = "umeng_share",
+        description = "友盟分享 SDK（真机）",
+        path        = "samples/umeng_share",
+    },
+
+    {
+        title       = "umeng_update",
+        description = "友盟更新 SDK（真机）",
+        path        = "samples/umeng_update",
+    },
+
+    {
         title       = "websockets",
-        description = "演示如何创建可触摸的对象",
+        description = "WebScokets 演示",
         path        = "samples/websockets",
     },
 
