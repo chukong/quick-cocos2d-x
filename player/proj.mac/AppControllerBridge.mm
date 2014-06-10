b84937a6ec532fb9ad6b7959eee31b2caa97d835
yinjimmy <jimmy.yin5@gmail.com>
2014-06-10 11:22:19 +0800

    mac player

---
+++ player/proj.mac/AppControllerBridge.mm
@@ -0,0 +1,28 @@
+
+#include "AppControllerBridge.h"
+#import "AppController.h"
+
+void AppControllerBridge::onWelcomeNewProject(CCObject *object)
+{
+    [(AppController*)m_controller welcomeNewProject];
+}
+
+void AppControllerBridge::onWelcomeOpen(CCObject *object)
+{
+    [(AppController*)m_controller welcomeOpen];
+}
+
+void AppControllerBridge::onWelcomeSamples(CCObject *object)
+{
+    [(AppController*)m_controller welcomeSamples];
+}
+
+void AppControllerBridge::onWelcomeGetStarted(CCObject *object)
+{
+    [(AppController*)m_controller welcomeGetStarted];
+}
+
+void AppControllerBridge::onWelcomeGetCommunity(CCObject *object)
+{
+    [(AppController*)m_controller welcomeCommunity];
+}
