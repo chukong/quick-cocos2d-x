8e709362202c342e863097804303ace09716022f
wland <83752170@qq.com>
2014-07-30 13:20:09 +0800

    add api : device.vibrate()

--- framework/device.lua
+++ framework/device.lua
@@ -49,6 +49,13 @@ THE SOFTWARE.
 -   device.directorySeparator 目录分隔符，在 Windows 平台上是 “\”，其他平台都是 “/”
 -   device.pathSeparator 路径分隔符，在 Windows 平台上是 “;”，其他平台都是 “:”
 
+-   android 平台下需要在 main Activity 将PSNative初始化: 
+    public void onCreate(Bundle savedInstanceState) {
+        PSNative.init(this);
+
+        ...
+    }
+
 ]]
 local device = {}
 
@@ -295,6 +302,37 @@ function device.showInputBox(title, message, defaultValue)
         printInfo("    defaultValue: %s", tostring(defaultValue))
     end
     return CCNative:getInputText(title, message, defaultValue)
+end
+
+
+--[[--
+
+震动
+
+@param int millisecond 震动时长(毫秒) (设置震动时长仅对android有效，默认200ms) 
+
+android 需要添加震动服务权限
+<uses-permission android:name="android.permission.VIBRATE" />  
+
+
+]]
+
+function device.vibrate(millisecond)
+    if DEBUG > 1 then
+        printInfo("device.vibrate(%s)", millisecond or "")
+    end
+ 
+    if device.platform == "android" then
+        if millisecond then
+            luaj.callStaticMethod("org/cocos2dx/utils/PSNative", "vibrate", {millisecond}, "(I)V");
+        else      
+            CCNative:vibrate()
+        end     
+    elseif device.platform == "ios" then
+        CCNative:vibrate()
+    else
+        printInfo("%s platform unsupporte vibrate", device.platform)
+    end
 end
 
 return device
