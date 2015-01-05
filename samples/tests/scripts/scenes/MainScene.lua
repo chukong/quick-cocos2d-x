0f43c994d98e1ee10a191d95e7e74ee112b09f06
SunLightJuly <ygqy8181@qq.com>
2015-01-05 16:54:26 +0800

    fixed #508:  modified socket scripts; added socket tcp test

--- samples/tests/scripts/scenes/MainScene.lua
+++ samples/tests/scripts/scenes/MainScene.lua
@@ -9,6 +9,7 @@ function MainScene:ctor()
         "framework.crypto",
         "framework.network",
         "framework.luabinding",
+        "framework.socketTcp",
     }
 
     self:addChild(game.createMenu(items, handler(self, self.openTest)))
